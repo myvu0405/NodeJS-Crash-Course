f2667cec2344e816dfb40c04bfb55efbb33c5808
My Vu <96148401+myvu0405@users.noreply.github.com>
2022-01-13 23:07:46 +0100

    Update app.js
    removed db connection info

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { result } = require('lodash');
 const app=express();
 
 //connect to mongodb
-const dbURI='mongodb+srv://myvu01:haydoiday1@nodetuts.zqauy.mongodb.net/node-tuts?retryWrites=true&w=majority';
+const dbURI='mongodb+srv://<username>:<password>@nodetuts.zqauy.mongodb.net/<clustername>?retryWrites=true&w=majority';
 mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology:true})
 .then((result)=> app.listen(3000))
 .catch((err) => console.log(err));
